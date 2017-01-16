b73c93f465bd05980009480ebde250a9fd8708ff
shtigran <shakhbekyantigran@gmail.com>
2017-01-16 12:48:00 +0400

    Double converter from byte array

---
+++ ConvertByteArrayToInt64/ConvertByteArrayToInt64/ConvertByteArrayToInt64.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace ConvertByteArrayToInt64
+{
+    public class Converter
+    {
+
+    static public double MyBitToInt64(byte[] arr)
+    {
+      StringBuilder strbuild = new StringBuilder();
+      for (int i = 0; i < 4; i++)
+      {
+        strbuild.Append(arr[i]);
+      }
+      return Convert.ToInt64(strbuild.ToString());
+    }  
+  }
+}
---
+++ ConvertByteArrayToInt64/Program/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+using ConvertByteArrayToInt64;
+
+namespace ConvertByteArrayToInt64
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      byte[] m = new byte[4];
+      RNGCryptoServiceProvider crypto = new RNGCryptoServiceProvider();
+      crypto.GetBytes(m);
+      double exampl = Converter.MyBitToInt64(m);
+      Console.WriteLine($"The double is: {exampl}");
+      Console.ReadKey();
+    }
+  }
+}
